e2918ac5819414dab0cd51ebe833a044087f350f
Pascal Friederich <pascal.friederich@xing.com>
2009-06-11 15:49:47 +0200

    hook now passes argv to its delegates new methods

--- lib/hooker/hook.rb
+++ lib/hooker/hook.rb
@@ -2,7 +2,7 @@ module Hooker
   class Hook
     def initialize(hook_type)
       delegate_class = "#{hook_type.to_s.capitalize}Hook"
-      @delegate = Object.module_eval("#{delegate_class}", __FILE__, __LINE__).new
+      @delegate = Object.module_eval("Hooker::#{delegate_class}", __FILE__, __LINE__).new(ARGV)
     end
   end
 end

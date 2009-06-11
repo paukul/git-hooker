b672b76803d4c374e02565822155ef866c0974c4
Pascal Friederich <pascal.friederich@xing.com>
2009-06-11 10:29:42 +0200

    some basic bootstrapping and test prepares

---
+++ lib/hooker.rb
@@ -0,0 +1,3 @@
+require 'rubygems'
+require 'grit'
+include Grit
---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'hooker'
+
+# This allows you to use the 'test "something should behave that way" do' syntax for plain test/unit tests
+# See ActiveSupport::Testing::Declarative:
+# http://github.com/rails/rails/blob/master/activesupport/lib/active_support/testing/declarative.rb
+def test(name, &block)
+  test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+  defined = instance_method(test_name) rescue false
+  raise "#{test_name} is already defined in #{self}" if defined
+  if block_given?
+    define_method(test_name, &block)
+  else
+    define_method(test_name) do
+      flunk "No implementation provided for #{name}"
+    end
+  end
+end

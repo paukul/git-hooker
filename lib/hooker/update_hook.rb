4898ed378360938a693183257daf96453729c263
Pascal Friederich <paukul@gmail.com>
2009-06-14 17:25:43 +0200

    wrapping commits in hooker commits

--- lib/hooker/update_hook.rb
+++ lib/hooker/update_hook.rb
@@ -7,7 +7,7 @@ module Hooker
     end
     
     def commits
-      repository.commits_between @old_revision_hash, @new_revision_hash
+      @commits ||= Hooker::Commit.from_commits(repository.commits_between(@old_revision_hash, @new_revision_hash))
     end
   end
 end
